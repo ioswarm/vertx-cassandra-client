a6b3b3d51fa709074ccc6237671ef081d66c1a78
apape <apape@000616D>
2016-11-25 17:39:24 +0100

    ...

--- src/test/java/ioswarm/vertx/ext/cassandra/CassandraTest.java
+++ src/test/java/ioswarm/vertx/ext/cassandra/CassandraTest.java
@@ -6,46 +6,46 @@ import io.vertx.core.json.JsonObject;
 
 public class CassandraTest extends CassandraTestBase {
 
-	@Test
-	public void testConnectionToLocalhost() throws Exception {
-		CassandraClient client = CassandraClient.createShared(vertx, new JsonObject());
-		try {
-			assertNotNull(client.cluster());
-			assertNotNull(client.session());
-			client.execute("create table user (username text, birthdate timestamp, primary key (username))", handler -> {
-				 if (handler.failed()) {
-					 System.err.println("ERROR at create table");
-					 handler.cause().printStackTrace();
-				 }
-				 
-				 client.execute("insert into user (username) values ('andreas')", handler2 -> {
-						if (handler2.failed()) {
-							System.err.println("ERROR at insert into");
-							handler2.cause().printStackTrace();
-						}
-						
-						client.query("select * from user", handler3 -> {
-							if (handler3.succeeded()) {
-								for (JsonObject jo : handler3.result()) {
-									System.out.println(jo.toString());
-								}
-									
-							} else {
-								System.err.println("ERROR at select");
-								handler3.cause().printStackTrace();
-							}
-							testComplete();
-						});
-						
-					});
-				 
-			});	
-			
-			await();
-		} finally {
-			client.close();
-		}
-		
-	}
+//	@Test
+//	public void testConnectionToLocalhost() throws Exception {
+//		CassandraClient client = CassandraClient.createShared(vertx, new JsonObject());
+//		try {
+//			assertNotNull(client.cluster());
+//			assertNotNull(client.session());
+//			client.execute("create table user (username text, birthdate timestamp, primary key (username))", handler -> {
+//				 if (handler.failed()) {
+//					 System.err.println("ERROR at create table");
+//					 handler.cause().printStackTrace();
+//				 }
+//				 
+//				 client.execute("insert into user (username) values ('andreas')", handler2 -> {
+//						if (handler2.failed()) {
+//							System.err.println("ERROR at insert into");
+//							handler2.cause().printStackTrace();
+//						}
+//						
+//						client.query("select * from user", handler3 -> {
+//							if (handler3.succeeded()) {
+//								for (JsonObject jo : handler3.result()) {
+//									System.out.println(jo.toString());
+//								}
+//									
+//							} else {
+//								System.err.println("ERROR at select");
+//								handler3.cause().printStackTrace();
+//							}
+//							testComplete();
+//						});
+//						
+//					});
+//				 
+//			});	
+//			
+//			await();
+//		} finally {
+//			client.close();
+//		}
+//		
+//	}
 	
 }
